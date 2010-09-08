b97d27017b6ddb3a88c7ee98742b27d25773935b
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2010-09-08 11:50:46 +0200

    fix nesting

--- lib/sinatra/extension.rb
+++ lib/sinatra/extension.rb
@@ -77,36 +77,36 @@ module Sinatra
     def sinatra_application
       @sinatra_application ||= MethodRecorder.new method_calls
     end
-  end
 
-  module CoreExt
-    module Object
-      ::Object.send(:include, self)
-      # Leaves the decission to use instance_exec for internal DSLs up the
-      # end user by using instance_exec if the given block takes more
-      # arguments than passed to instance_yield, yield(self, *args) otherwise.
-      #
-      # @example
-      #   def my_dsl(&block)
-      #     dsl_object.instance_yield(42, &block)
-      #   end
-      #   
-      #   my_dsl do |number|
-      #     do_something :with => "a string"
-      #     self # => dsl_object
-      #     number # => 42
-      #   end
-      #   
-      #   my_dsl do |object, number|
-      #     object.do_something :with => "a string"
-      #     self # => main
-      #     number # => 42
-      #   end
-      def instance_yield(*args, &block)
-        if block.arity > args.size or (-1 - block.arity) > args.size
-          yield(self, *args)
-        else
-          instance_exec(*args, &block)
+    module CoreExt
+      module Object
+        ::Object.send(:include, self)
+        # Leaves the decission to use instance_exec for internal DSLs up the
+        # end user by using instance_exec if the given block takes more
+        # arguments than passed to instance_yield, yield(self, *args) otherwise.
+        #
+        # @example
+        #   def my_dsl(&block)
+        #     dsl_object.instance_yield(42, &block)
+        #   end
+        #   
+        #   my_dsl do |number|
+        #     do_something :with => "a string"
+        #     self # => dsl_object
+        #     number # => 42
+        #   end
+        #   
+        #   my_dsl do |object, number|
+        #     object.do_something :with => "a string"
+        #     self # => main
+        #     number # => 42
+        #   end
+        def instance_yield(*args, &block)
+          if block.arity > args.size or (-1 - block.arity) > args.size
+            yield(self, *args)
+          else
+            instance_exec(*args, &block)
+          end
         end
       end
     end
